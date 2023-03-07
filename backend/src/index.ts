c7f8831766b6f8679ee9ad0233fb40303c0a6c78
Paragkoche <paragkoche0@gmail.com>
2023-03-07 09:29:17 +0530

    add Readme file and add routes (endpoints)

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,31 @@
+//*------------> imports
 import express from "express";
+import CookieParser from "cookie-parser"
+import {adminRouter,paymentRouter,serviceRouter,userRouter} from "./Routers"
+
+
+
+//*------------> configures
 const app = express();
+app.use(express.json());
+app.use(CookieParser())
+
+
+
+//*-----------> Routers 
+app.use('/user',userRouter)
+app.use('/service',serviceRouter)
+app.use('/payment',paymentRouter)
+app.use('/admin',adminRouter)
+
+
+
+//*----------> Server Ping Pog
 app.get('/ping',(req,res)=>{
-    res.send({'ping':'ok'})
+    res.send({'pog':'ok'})
 })
+
+
+
+//*-------> Server start 
 app.listen(8080,()=>console.log(`server start on http://localhost:8080`))
